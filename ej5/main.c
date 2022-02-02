fec3fa782d65ec3de490a7c9c6e4acee9aaca7c7
aiitorvm <95426178+aiitorvm@users.noreply.github.com>
2022-02-02 16:57:32 +0100

    Add files via upload

---
+++ ej5/main.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "functions.h"
+// EJERCICIO 5
+void main(){
+	int num1, num2, aux;
+	num1 = validate();
+	num2 = validate();
+	aux = num1;
+	num1 = num2;
+	num2 = aux;
+	printf("El primer número es el %d y el segundo número es el %d", num1, num2);
+}
+
